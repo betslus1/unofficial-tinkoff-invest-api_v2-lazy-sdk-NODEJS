859ccce4fec38fa96bc2e1f1b0fb119e3d75ee30
Vitaliy Slyusarenko <vitaliy@slus.name>
2022-03-12 19:52:04 +0300

    пример с преобразованием типов request

---
+++ example/getOperations.js
@@ -0,0 +1,11 @@
+let from = new Date();
+let to   = new Date();
+to.setHours(to.getHours() - 24);
+
+let operations = await module.exports.Operations.GetOperations({
+	'account_id' : opt.account,
+	'state' : "OPERATION_STATE_EXECUTED",
+	'from'  : from,
+	'to'    : to,
+	'figi'  : opt.figi,
+});
