df7b7773c93871f0ec37fe7d3393e0f5feb3caaf
bet <vitaliy@slus.name>
2022-03-12 19:57:11 +0300

    Update getOperations.js

--- example/getOperations.js
+++ example/getOperations.js
@@ -1,14 +1,11 @@
-let from = new Date();
-let to   = new Date();
-to.setHours(to.getHours() - 24);
-
 var tinkoff_v2 = require ('../tinkoff_v2.js');
-
-var api = new tinkoff_v2({
-	'token'   : process.argv[2]
-});
+var api = new tinkoff_v2({'token'   : process.argv[2]});
 
 (async function main(){	
+	let from = new Date();
+	let to   = new Date();
+	to.setHours(to.getHours() - 24);
+	
 	let operations = await api.Operations.GetOperations({
 		'account_id' : "12345",
 		'state' : "OPERATION_STATE_EXECUTED",
