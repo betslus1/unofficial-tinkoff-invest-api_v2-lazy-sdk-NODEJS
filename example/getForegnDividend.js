2aaa5695e2af9b0b27ce59a7ff09958e2ce47ca6
bet <vitaliy@slus.name>
2022-04-25 20:34:09 +0300

    Update getForegnDividend.js

--- example/getForegnDividend.js
+++ example/getForegnDividend.js
@@ -11,12 +11,21 @@ var api = new tinkoff_v2({'token'   : process.argv[2]});
       "from": from,
       "to": to
     },
-    //Автоматически не декодируются вложенные объекты, для них вручную указываем типы данных
+    //Автоматически не кодируются вложенные объекты, для них вручную указываем типы данных
     'rules':{
       'from' : 'google.protobuf.Timestamp',
       'to'   : 'google.protobuf.Timestamp'
     }
   });
 
-  console.log(operationNow);
+  //Автоматически могут не распозанваться глубоко вложенные объекты, надо помочь)
+  console.log(api.decodeResponse(operationNow?.div_foreign_issuer_report?.dividends_foreign_issuer_report, {
+      'record_date' : 'google.protobuf.Timestamp',
+      'payment_date'   : 'google.protobuf.Timestamp',
+      'dividend'   : 'Quotation',
+      'external_commission'   : 'Quotation',
+      'dividend_gross'   : 'Quotation',
+      'tax'   : 'Quotation',
+      'dividend_amount'   : 'Quotation',
+  });
 })();
